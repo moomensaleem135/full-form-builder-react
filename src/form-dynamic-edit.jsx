d115ba6475962e6d0a768e3bbdfbfb1b9d666733
moomensaleem135 <moomensaleem135@gmail.com>
2024-09-02 14:53:27 +0500

    chagnes

--- src/form-dynamic-edit.jsx
+++ src/form-dynamic-edit.jsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const FormElementsEditor = (props) => {
-  const [
-    dynamic,
-    setDynamic,
-  ] = React.useState(null);
+  const [dynamic, setDynamic] = React.useState(null);
 
   React.useEffect(() => {
     const loadDynamic = async () => {
-      const x = await import('./form-elements-edit');
+      const x = await import("./form-elements-edit");
       setDynamic(() => x.default);
     };
     loadDynamic();
   }, []);
 
   if (!dynamic) {
-    return (<div>Loading...</div>);
+    return <div>Loading...</div>;
   }
   const Component = dynamic;
-  return <Component {...props}/>;
+  return (
+    <div >
+      <div style={{width:'100%',display:'flex',justifyContent:'end'}}>
+
+      <button style={{background:'none',borderRadius:'50%',border:'none',fontSize:'1.1rem'}} onClick={()=>props.manualEditModeOff()} >
+        X
+      </button>
+      </div>
+      <Component {...props} />;
+    </div>
+  );
 };
 
 export default FormElementsEditor;
